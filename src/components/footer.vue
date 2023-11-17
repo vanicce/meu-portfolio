9f5586f1365ca78adf2f0c7f3725f65ffe2fa62e
Lucas <ssilvalucas2004@gmail.com>
2023-11-17 01:07:00 -0300

    feat: creating footer component

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,15 @@
 <template>
-  <footer class="w-full">
-    <p>Entre em contato: <a href="" target="_blank">seuemail@example.com</a></p>
+  <footer class="bg-stone-100 w-full flex flex-col items-center border-t border-gray-300">
+    <h2 class="text-lg font-semibold">contact me: </h2>
+    <div class="flex items-center justify-center gap-2">
+      <a class="transition hover:scale-110" href="https://github.com/lucwx" target="_blank">
+        <img class="w-10" src="../assets/github.svg" alt="">
+      </a>
+      <a class="transition hover:scale-110" href="https://linkedin.com/in/pedro-lucas-santos-silva" target="_blank">
+        <img class="w-14" src="../assets/linkedin.svg" alt="">
+      </a>
+    </div>
+    <a class="underline" href="mailto:ssilvalucas2004@gmail.com?subject=Olá, vi seu portfólio"
+      target="_blank">ssilvalucas2004@gmail.com</a>
   </footer>
 </template>
