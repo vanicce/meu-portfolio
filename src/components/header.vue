1923046b3bd2fae27a0f06b3f67338fb4bd20f8f
Lucas <ssilvalucas2004@gmail.com>
2023-11-17 01:07:27 -0300

    feat: creating header component

--- src/components/header.vue
+++ src/components/header.vue
@@ -9,11 +9,11 @@ const openMenu = () => {
 </script>
 
 <template>
-  <header class="bg-stone-100 sticky top-0 left-0 flex flex-col w-full">
+  <header class="bg-stone-100 sticky top-0 left-0 flex flex-col w-full border-b border-gray-300">
     <div class="flex justify-between items-center w-full p-3">
       <p class="font-semibold">Lucas S. Silva</p>
       <button class="transition ease-in-out hover:scale-110" @click="openMenu()">
-        <img class="" width="30" src="../assets/icons8-cardápio.svg" alt="menu">
+        <img class="" width="25" src="../assets/icons8-cardápio.svg" alt="menu">
       </button>
     </div>
     <nav class="duration-[400ms] ease-in-out absolute bg-stone-100 w-full flex flex-col items-center gap-2 p-2" :class="[open ? 'top-12 ' : '-top-96']">
