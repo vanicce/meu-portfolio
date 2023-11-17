37fd8a2ad5f454457bb2717eb69adbad377fb4bd
Lucas <ssilvalucas2004@gmail.com>
2023-11-17 01:06:32 -0300

    feat: creating floating button

---
+++ src/components/floatingbutton.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+let scroll = ref(false)
+
+const scrolling = () => {
+  document.body.scrollTop > 20 || document.documentElement.scrollTop > 20 ? scroll.value = true : scroll.value = false
+}
+
+window.onscroll = () => { scrolling() }
+
+const toTop = () => {
+  document.documentElement.scrollTop = 0
+}
+</script>
+
+<template >
+  <button @click="toTop()"
+    class="duration-500 ease-in-out border border-black rounded-full w-12 fixed bottom-10  backdrop-blur-sm"
+    :class="scroll ? 'left-[80%] md:left-[90%] lg:left-[90%] xl:left-[95%]' : 'left-full'">
+    <img src="../assets/arrow.svg" class="" alt="">
+  </button>
+</template>
