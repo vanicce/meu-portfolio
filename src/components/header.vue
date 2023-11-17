5ae354805274db4887241aa715a2249de8e776f2
Lucas Silva <ssilvalucas2004@gmail.com>
2023-11-17 15:34:08 -0300

    feat: improving button from header component

--- src/components/header.vue
+++ src/components/header.vue
@@ -13,10 +13,10 @@ const openMenu = () => {
     <div class="flex justify-between items-center w-full p-3">
       <p class="font-semibold">Lucas S. Silva</p>
       <button class="transition ease-in-out hover:scale-110" @click="openMenu()">
-        <img class="" width="25" src="../assets/icons8-cardápio.svg" alt="menu">
+        <img width="25" :src="[open ? './src/assets/close.svg' : './src/assets/menu.svg']" alt="menu">
       </button>
     </div>
-    <nav class="duration-[400ms] ease-in-out absolute bg-stone-100 w-full flex flex-col items-center gap-2 p-2" :class="[open ? 'top-12 ' : '-top-96']">
+    <nav class="duration-[400ms] ease-in-out absolute bg-stone-100 border-b border-gray-300 w-full flex flex-col items-center gap-2 p-2" :class="[open ? 'top-12 ' : '-top-96']">
       <routerLink class="underline" @click="openMenu()" to="/">Home</routerLink>
       <routerLink class="underline" @click="openMenu()" to="/about">About Me</routerLink>
       <router-link class="underline" @click="openMenu()"  to="/projects">Projects</router-link>
