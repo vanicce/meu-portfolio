d87c3b0afc7355d1cde128937eae7cac64c9e566
Lucas <ssilvalucas2004@gmail.com>
2023-11-17 01:08:13 -0300

    feat: creating HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,17 @@
 <script setup>
+let spotify = "https://open.spotify.com/intl-pt/artist/5INjqkS1o8h1imAzPqGZBb"
 </script>
 
 <template>
-  <main class="h-screen p-4">
-    <div class="">
-      <h1 class="text-4xl font-bold">Hi, I'm Lucas, I'm 19 years old and I'm a web developer </h1>
+  <main class="flex flex-col items-center h-full min-h-screen p-4">
+    <div class="w-56 h-56 text-4xl font-bold overflow-clip rounded-full my-3">
+      <img class="" src="../assets/eu3.jpeg" alt="">
     </div>
-    <p>I love programming, games and <a class="underline" target="_blank" href="https://open.spotify.com/intl-pt/artist/5INjqkS1o8h1imAzPqGZBb">Tame Impala</a></p>
+    <div class="text-2xl md:text-4xl font-black text-center">
+      <h1 class="">Hi, my name is Lucas.</h1>
+    </div>
+    <p class="text-lg md:text-2xl text-center">I love programming, games and
+    <a class="underline decoration-slate-500 bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500" target="_blank" :href="spotify">Tame Impala.</a> Here you will find some projects and learn a little about me.</p>
+    <router-link class="border border-black rounded-md p-2 m-4 transition hover:scale-110" to="/about">Enjoy!</router-link>
   </main>
 </template>
-
