70d695223df8f1e3df3704c4d1fd3a2228f13564
Lucas <ssilvalucas2004@gmail.com>
2023-11-20 22:14:58 -0300

    feat: adding transition animations to routes

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,26 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <Header class="fixed top-0"/>
-    <RouterView class="h-full min-h-screen pt-14 bg-stone-100"/>
-    <floatingbutton class="z-50"/>
+  <Header class="fixed top-0" />
+  <transition name="slide-fade">
+    <RouterView class="h-full min-h-screen pt-14 bg-stone-100" />
+  </transition>
+  <floatingbutton class="z-50" />
   <Footer />
 </template>
+
+<style>
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+</style>
