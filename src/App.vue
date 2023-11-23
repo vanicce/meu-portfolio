8434641199b47c6a793c94752a932359388c9146
Lucas <ssilvalucas2004@gmail.com>
2023-11-22 21:53:02 -0300

    feat: rewrite header.vue to more beauty

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,13 @@
 <script setup>
 import Header from './components/header.vue';
-import Footer from './components/footer.vue';
 import floatingbutton from './components/floatingbutton.vue'
 import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
-  <Header class="fixed top-0" />
-  <transition name="slide-fade">
-    <RouterView class="h-full min-h-screen pt-14 bg-stone-100" />
-  </transition>
-  <floatingbutton class="z-50" />
-  <Footer />
+  <Header class="flex justify-center fixed top-0 inset-x-0 z-50 px-6 h-12 mt-4" />
+  <RouterView class="h-full min-h-[100dvh] bg-stone-100 py-14" />
+  <floatingbutton />
 </template>
 
-<style>
-.slide-fade-enter-active {
-  transition: all 0.3s ease-out;
-}
-
-.slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
-}
-
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  transform: translateX(20px);
-  opacity: 0;
-}
-</style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,25 +1,9 @@
-<script setup>
-import { ref } from 'vue'
-
-let open = ref(false)
-
-const openMenu = () => {
-  open.value = !open.value
-}
-</script>
-
 <template>
-  <header class="bg-stone-100 flex flex-col w-full border-b border-gray-300 z-50 md:flex-row md:justify-between">
-    <div class="flex justify-between items-center w-full p-3 md:p-1">
-      <p class="font-semibold md:w-2/4">Lucas S. Silva</p>
-      <button class="md:hidden transition ease-in-out hover:scale-110" @click="openMenu()">
-        <img width="25" :src="open ? './closemenu.svg' : './menu.svg'" alt="menu">
-      </button>
-      <nav class="duration-[400ms] ease-in-out absolute bg-stone-100 border-b border-gray-300 w-full flex flex-col items-center gap-2 p-2 md:flex-row md:relative md:border-none md:justify-end md:text-lg" :class="[open ? 'top-12 md:top-auto' : '-top-96 md:top-auto']">
-        <router-link class="underline" @click="openMenu()" to="/">Home</router-link>
-        <router-link class="underline" @click="openMenu()" to="/about">About Me</router-link>
-        <router-link class="underline" @click="openMenu()"  to="/projects">Projects</router-link>
-      </nav>
+  <div >
+    <div class="flex h-full items-center gap-3 border border-gray-500/20 rounded-[14px] backdrop-blur-sm px-3 bg-white/40">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/projects">Projects</router-link>
     </div>
-  </header>
+  </div>
 </template>
