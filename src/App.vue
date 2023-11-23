93d1cb3c618401523895d6a93f83e0138b8bc70f
Lucas <ssilvalucas2004@gmail.com>
2023-11-22 23:49:54 -0300

    feat: fixing responsive design

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import { RouterView } from 'vue-router'
 
 <template>
   <Header class="flex justify-center fixed top-0 inset-x-0 z-50 px-6 h-12 mt-4" />
-  <RouterView class="h-full min-h-[100dvh] bg-stone-100 py-14" />
+  <RouterView class="h-full min-h-[100dvh] bg-stone-100 py-16" />
   <floatingbutton />
 </template>
 
