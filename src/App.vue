2e8031996f7e032e7695f073bea3e8d79644e98c
Lucas Silva <ssilvalucas2004@gmail.com>
2023-11-24 18:02:01 -0300

    feat: adding animations in pages

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,28 @@
 <script setup>
 import Header from './components/header.vue';
 import floatingbutton from './components/floatingbutton.vue'
-import { RouterView } from 'vue-router'
 
 </script>
 
 <template>
   <Header class="flex justify-center fixed top-0 inset-x-0 z-50 px-6 h-12 mt-4" />
-  <RouterView class="h-full min-h-[100dvh] bg-stone-100 py-16" />
+  <router-view class="h-full min-h-[100dvh] bg-stone-100 py-16" v-slot="{ Component }">
+    <transition name="fade" >
+      <component :is="Component" />
+    </transition>
+  </router-view>
   <floatingbutton />
 </template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
 
