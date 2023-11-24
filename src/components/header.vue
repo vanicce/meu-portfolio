b92cd8641d6574fb1f63ddb369ab54ef5e7fcdb9
Lucas Silva <ssilvalucas2004@gmail.com>
2023-11-24 18:02:23 -0300

    feat: adding hover underline to router links

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,9 +1,34 @@
 <template>
   <div >
     <div class="flex h-full items-center gap-3 border border-gray-500/20 rounded-[14px] backdrop-blur-sm px-3 bg-white/40">
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-      <router-link to="/projects">Projects</router-link>
+      <router-link class="element" to="/">Home</router-link>
+      <router-link class="element" to="/about">About</router-link>
+      <router-link class="element" to="/projects">Projects</router-link>
     </div>
   </div>
 </template>
+
+<style scoped>
+.element {
+  position: relative;
+  display: inline-block;
+  color: #333; /* Cor do texto */
+}
+
+.element::after {
+  content: '';
+  display: block;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 2px; /* Espessura do underline */
+  background-color: #1e1e1f; /* Cor do underline normal */
+  transform: scaleX(0); /* Inicialmente, o underline está invisível */
+  transition: transform 0.3s ease; /* Adiciona uma transição suave */
+}
+
+.element:hover::after {
+  transform: scaleX(1); /* Quando o mouse está sobre o elemento, o underline aparece */
+}
+</style>
