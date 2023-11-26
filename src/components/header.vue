b9e2f730a3fa84879f1a153e6b87ff8ce32d43be
Lucas <ssilvalucas2004@gmail.com>
2023-11-26 15:42:35 -0300

    feat: improving header

--- src/components/header.vue
+++ src/components/header.vue
@@ -11,31 +11,23 @@ const toggleTheme = () => {
 
 <template>
   <div >
-    <div class="flex h-full items-center gap-3 border border-gray-500/20 rounded-[14px] backdrop-blur-sm px-3 bg-white/40">
-      <router-link class="element" to="/">Home</router-link>
-      <router-link class="element" to="/about">About</router-link>
-      <router-link class="element" to="/projects">Projects</router-link>
-      <button class="border-l pl-2 border-[#333] transition" @click="toggleTheme"><img width="19" :src="isDark ? './sun.svg' : './moon.svg'"></button>
+    <div class="flex h-full items-center gap-3 border border-gray-500/20 rounded-[14px] backdrop-blur-sm px-3 text-[#333] bg-white/40 dark:text-neutral-200 dark:bg-[rgba(18,17,19,.4)]">
+      <router-link class="element relative after:bg-[#333] dark:after:bg-neutral-50 " to="/">Home</router-link>
+      <router-link class="element relative after:bg-[#333] dark:after:bg-neutral-50 " to="/about">About</router-link>
+      <router-link class="element relative after:bg-[#333] dark:after:bg-neutral-50 " to="/projects">Projects</router-link>
+      <button class="border-l pl-2 border-[#333] dark:border-neutral-200 transition" @click="toggleTheme"><img width="19" :src="isDark ? './sun.svg' : './moon.svg'"></button>
     </div>
   </div>
 </template>
 
 <style scoped>
-.element {
-  position: relative;
-  display: inline-block;
-  color: #333;
-}
-
 .element::after {
-  content: '';
   display: block;
   position: absolute;
   left: 0;
   bottom: 0;
   width: 100%;
   height: 1px;
-  background-color: #333;
   transform: scaleX(0);
   transition: transform 0.3s ease;
 }
