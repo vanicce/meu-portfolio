71c383cd60d2a99dffb66222540aee1bdf0fd498
Lucas <ssilvalucas2004@gmail.com>
2023-11-25 22:05:51 -0300

    feat: creating dark theme mode

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,9 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+let isDark = ref(false)
+
+const toggleTheme = () => {
+  isDark.value = !isDark.value
+  document.documentElement.classList.toggle('dark', isDark.value)
+}
+</script>
+
 <template>
   <div >
     <div class="flex h-full items-center gap-3 border border-gray-500/20 rounded-[14px] backdrop-blur-sm px-3 bg-white/40">
       <router-link class="element" to="/">Home</router-link>
       <router-link class="element" to="/about">About</router-link>
       <router-link class="element" to="/projects">Projects</router-link>
+      <button class="border-l pl-2 border-[#333] transition" @click="toggleTheme"><img width="19" :src="isDark ? './sun.svg' : './moon.svg'"></button>
     </div>
   </div>
 </template>
@@ -12,7 +24,7 @@
 .element {
   position: relative;
   display: inline-block;
-  color: #333; /* Cor do texto */
+  color: #333;
 }
 
 .element::after {
@@ -22,15 +34,15 @@
   left: 0;
   bottom: 0;
   width: 100%;
-  height: 2px; /* Espessura do underline */
-  background-color: #333; /* Cor do underline normal */
-  transform: scaleX(0); /* Inicialmente, o underline está invisível */
-  transition: transform 0.3s ease; /* Adiciona uma transição suave */
+  height: 1px;
+  background-color: #333;
+  transform: scaleX(0);
+  transition: transform 0.3s ease;
 }
 
 .element:hover::after,
 a.router-link-active::after {
-  transform: scaleX(1); /* Quando o mouse está sobre o elemento, o underline aparece */
+  transform: scaleX(1);
 }
 
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./src/components/*.vue",
     "./src/views/*.vue",
     "./*.html",
     "./src/*.vue",
+    "./src/assets/*.svg",
+    "./public/*.svg"
   ],
   theme: {
     extend: {
