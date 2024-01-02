60d7b3465cfd6be7caaa6b639fc3fb7982402f4d
Lucas <ssilvalucas2004@gmail.com>
2024-01-02 02:08:08 -0300

    feat: improving readability of aboutview.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,31 +7,40 @@ const techs = ['HTML5', 'CSS3', 'JavaScript', 'vue', 'react', 'tailwind', 'mySQL
 </script>
 
 <template>
-  <main class="flex flex-col dark:text-[#BBBBBB]">
-    <section class="flex justify-center items-center flex-col p-3 md:px-16 gap-2">
+  <main class="flex flex-col dark:text-[#BBBBBB] lg:px-20">
+    <section class="flex justify-center items-center flex-col p-3 gap-4">
       <h2 class="text-shark text-xl font-bold md:text-4xl dark:text-[#BBBBBB]">About Me</h2>
-      <p class="md:text-lg md:w-full">I'm Lucas, an aspiring programmer on a <strong>continuous learning journey</strong>. Currently, I'm dedicated to honing my skills in programming and exploring the diverse realms of software development.</p>
+      <p class="md:text-xl md:w-full">I'm Lucas, an aspiring programmer on a <strong>continuous learning journey</strong>.
+        Currently, I'm dedicated to honing my skills in programming and exploring the diverse realms of software
+        development.</p>
 
-      <p class="md:text-lg">I am investing my time in studies, acquiring knowledge in <strong>JavaScript, Vue and Tailwind</strong>. My passion for problem-solving and creating innovative solutions drives me to delve deep into the programming universe.</p>
+      <p class="md:text-xl">I am investing my time in studies, acquiring knowledge in <strong>JavaScript, Vue and
+          Tailwind</strong>. My passion for problem-solving and creating innovative solutions drives me to delve deep into
+        the programming universe.</p>
+
     </section>
 
-    <section class="flex flex-col items-center ">
+    <section class="flex flex-col items-center gap-4">
       <h2 class="text-shark text-xl md:text-4xl font-bold dark:text-[#BBBBBB]">My Habilities:</h2>
       <div class="w-full md:w-2/4 overflow-hidden">
         <div class="scroller_inner flex flex-nowrap w-fit p-4 px-4 gap-10 ">
-          <scrollComponent v-for="tech in techs" :tech="tech"/>
-          <scrollComponent v-for="tech in techs" :tech="tech"/>
+          <scrollComponent v-for="tech in techs" :tech="tech" />
+          <scrollComponent v-for="tech in techs" :tech="tech" />
         </div>
       </div>
     </section>
 
-    <section class="md:px-16 flex flex-col">
-      <p class="p-3 md:text-lg">Throughout my learning journey, I have developed projects. Each project represents a significant step in my evolution as a programmer, showcasing my ability to apply learned concepts in <strong>practical scenarios.</strong></p>
+    <section class="flex flex-col items-center">
+      <p class="p-3 md:text-xl">Throughout my learning journey, I have developed projects. Each project represents a
+        significant step in my evolution as a programmer, showcasing my ability to apply learned concepts in
+        <strong>practical scenarios.</strong>
+      </p>
 
-      <p class="italic md:text-lg p-3 self-center">To see my projects click here!</p>
-      <router-link to="/projects" class="border p-2 rounded-md m-4 border-black text-center font-bold md:self-center md:text-lg transition md:hover:scale-110 dark:border-[#BBBBBB]">Projects</router-link>
+      <p class="italic md:text-xl p-1 self-center">To see my projects click here!</p>
+      <router-link to="/projects"
+        class="border p-2 rounded-md m-3 border-black text-center font-bold md:self-center md:text-xl transition md:hover:scale-110 dark:border-[#BBBBBB]">Projects</router-link>
 
-      <contact/>
+      <contact />
     </section>
   </main>
 </template>
@@ -49,6 +58,7 @@ const techs = ['HTML5', 'CSS3', 'JavaScript', 'vue', 'react', 'tailwind', 'mySQL
   from {
     transform: translate(0%);
   }
+
   to {
     transform: translate(-50%);
   }
